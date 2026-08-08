36574b8acd593860c71b7cbdc203be21bd0357cc
narayanathota <narayanathota420@gmail.com>
2026-08-08 17:07:48 +0530

    keywords,variable assignments

--- Day_2/keyowrds.py
+++ Day_2/keyowrds.py
@@ -1,5 +1,13 @@
+
 import keyword
 print(keyword.kwlist)
 print(len(keyword.kwlist))
 print(keyword.iskeyword)
 print(keyword.issoftkeyword
+
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+print(keyword.iskeyword)
+print(keyword.issoftkeyword
+
--- Day_2/variable assignments.py
+++ Day_2/variable assignments.py
@@ -52,3 +52,57 @@ NameError: name 'a' is not defined
 20
 >>> c
 30
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/naray/OneDrive/Desktop/python_course_work/Day_2/keyowrds.py
+['_', 'case', 'match', 'type']
+
+= RESTART: C:/Users/naray/OneDrive/Desktop/python_course_work/Day_2/keyowrds.py
+['_', 'case', 'match', 'type']
+
+= RESTART: C:/Users/naray/OneDrive/Desktop/python_course_work/Day_2/keyowrds.py
+['_', 'case', 'match', 'type']
+<built-in method __contains__ of frozenset object at 0x0000024AF4FDBCA0>
+
+= RESTART: C:/Users/naray/OneDrive/Desktop/python_course_work/Day_2/keyowrds.py
+['_', 'case', 'match', 'type']
+<built-in method __contains__ of frozenset object at 0x000001EA0D99BCA0>
+<built-in method __contains__ of frozenset object at 0x000001EA0D99BD80>
+
+= RESTART: C:/Users/naray/OneDrive/Desktop/python_course_work/Day_2/keyowrds.py
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+
+= RESTART: C:/Users/naray/OneDrive/Desktop/python_course_work/Day_2/keyowrds.py
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+<built-in method __contains__ of frozenset object at 0x00000235E4B9BCA0>
+<built-in method __contains__ of frozenset object at 0x00000235E4B9BCA0>
+SyntaxError: invalid syntax
+a=10
+a=b=c=10
+a
+10
+b
+10
+a,b,c=10,20,30
+a
+10
+>>> b
+20
+>>> c
+30
+>>> b=20
+>>> b
+20
+>>> del(a)
+>>> a
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+>>> b
+20
+>>> c
+30
