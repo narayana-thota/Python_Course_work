ac6c2d92e6fb6c1b11c087e5c12046ad7280010b
narayanathota <narayanathota420@gmail.com>
2026-08-10 23:31:47 +0530

    strings

---
+++ Day_6/Strings concept.py
@@ -0,0 +1,72 @@
+"""f=input("enter  string")
+l=input("enter l name")
+#string concatenation
+c=f+l
+print(c,sep='@')
+#string repetation
+print(c*5)
+
+#indexing
+names='narayana sameer vijay sailesh'
+print(names[0:8])
+print(names[::-1])
+print(names[9:16])
+print(names[-1:-8:-1])
+print("karthik" in names)
+print('narayana' not  in names)
+
+#functions of str
+name=" narayana"
+print(len(name))
+print(ord(name[1]))
+print(chr(90))
+print(sorted(name))
+print(min(name)
+
+#case methods in string
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+#align methods
+name="narayana"
+print(name.center(20,'-'))
+print(name.center(20,'$'))
+print(name.ljust(20,'&'))
+print(name.rjust(20,'&'))
+print('67'.zfill(3))
+
+s="python programmming"
+print(s.find('python'))
+print(s.find(' programming'))
+print(s.find('r'))
+print(s.rfind('r'))
+print(s.index("p"))
+print(s.rindex("p"))
+print(s.count("p"))
+print(s.replace('p','$'))
+print(s.replace('programming','language'))
+print(s.maketrans('aeiou','!@#$%'))
+print(s.translate({97: 33, 101: 64, 105: 35, 111: 36, 117: 37}))
+"""
+a="nothing 😊"
+
+print(a.encode())
+print(a.decode(b'nothing \xf0\x9f\x98\x8a'))
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+        
