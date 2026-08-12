e99f30a40fa36c8c55555c63a93effd6cd52b12e
narayanathota <narayanathota420@gmail.com>
2026-08-12 17:48:35 +0530

    list,tuple

---
+++ Day_7/Tuple.py
@@ -0,0 +1,44 @@
+'''t=()
+t1=(92,67,80)
+print(t1)
+t2=(4)
+print(t2)
+t3=(92.8,67,80,True,["narayana","thota"],{"name":"narayana"},{"narayana","narayana"})
+print(t3)
+
+t=(1,2,3,4,5)
+for i in t:
+    if i[1]==str(2):
+        t[1]=3
+print(t)
+
+t1=(1,2,3,4)
+t2=(5,6,7,8)
+print(t1+t2)
+print(t1*2)
+print(t1[-1::-1])
+print(t1[0::2])
+print(5 in t1)
+print(4 in t1)
+print(5 not in t1)
+print(4 not in t1)
+t=(5,6,7,8)
+print(sorted(t))
+print(max(t))
+print(len(t))
+print(min(t))
+print(t.index(6))
+print(all(t))
+print(any(t))
+t=(5,6,7,8,"narayana")
+print(all(t))
+print(any(t))
+'''
+t3=(92.8,67,80,True,["narayana","thota"],{"name":"narayana"},{"narayana","narayana"})
+print(t3[4])
+t3[4].append("mechease")
+print(t3)
+t=
+
+
+
