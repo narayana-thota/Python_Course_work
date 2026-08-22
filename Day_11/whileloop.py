9b6e545fd60974fb8c010377c2785141bd33772c
narayanathota <narayanathota420@gmail.com>
2026-08-22 18:04:40 +0530

    while loop

---
+++ Day_11/whileloop.py
@@ -0,0 +1,101 @@
+#while loop is exectuted until the while loop is satisfied
+'''
+there are total three conditions we have to consider when we are performing the while 
+1.intillization
+2.base condition
+3.iteration
+
+i=1
+while i<=10:
+    print(i)
+    i+=1
+    
+i=10
+while i>0:
+    print(i)
+    i-=1
+    
+i=5
+while i<=50:
+    print(i)
+    i+=5
+    
+s='while loop'
+i=0
+while i<len(s):
+    print(s[i])
+    i+=1
+    
+
+s='while loop'
+i=len(s)-1
+while i>=0:
+    print(s[i])
+    i-=1
+    
+lst=[75467,5678,6789,987]
+i=0
+while i<len(lst):
+    print(lst[i])
+    i+=
+    
+n=8756
+while n>0:
+    print(n%10)
+    n//=10
+print(s)
+
+n=8756
+s=1
+while n>0:
+    f=n%10
+    s=s+f
+    n//=10
+print(s)
+
+n=8756
+res=0
+while n>0:
+    rem=n%10
+    if 
+    res=res*10+rem
+    n//=10
+print(res)
+
+n=8756
+res=0
+while n>0:
+    rem=n%10
+    if rem%2==0:
+        res+=rem
+    n//=10
+print(res)
+ 
+print(l)
+
+l=[7,9,23,0,0,12,0,1,0,34,0,56]
+while 0 in l:
+    l.remove(0)
+print(l)
+'''
+l=[2,3,6,76,12,4,1,5,61,4,5,2,23]
+i,j=0,len(l)-1
+while i<=j:
+    if i==j:
+        print(l[i])
+    else:
+        print(l[i]+l[j])
+    i=i+1
+    j=j-1
+    
+
+
+
+
+
+
+
+
+
+
+
